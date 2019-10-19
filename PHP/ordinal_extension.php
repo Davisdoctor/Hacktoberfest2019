f87933594520af0498916ebf64ef93c1b73f6e02
Byamugisha Davis <davisb256@gmail.com>
2019-10-19 15:41:24 +0300

    Function to add (th, st, nd, rd, th) to end of number

---
+++ PHP/ordinal_extension.php
@@ -0,0 +1,10 @@
+
+// Add (th, st, nd, rd, th) to the end of a number
+
+function ordinal_extension($cdnl){ 
+    $test_c = abs($cdnl) % 10; 
+    $ext = ((abs($cdnl) %100 < 21 && abs($cdnl) %100 > 4) ? 'th' 
+            : (($test_c < 4) ? ($test_c < 3) ? ($test_c < 2) ? ($test_c < 1) 
+            ? 'th' : 'st' : 'nd' : 'rd' : 'th')); 
+    return $cdnl.$ext; 
+}
